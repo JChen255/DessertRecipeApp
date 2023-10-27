dcac352288da663fcbf4a0a66620fedb353a68b6
Jialin Chen <98052549+JChen255@users.noreply.github.com>
2023-10-26 20:24:43 -0400

    Add view models

---
+++ DessertRecipeApp/DessertRecipeApp/ViewModels/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  DessertRecipeApp
+//
+//  Created by Janine Chen on 10/26/23.
+//
+
+import Foundation
---
+++ DessertRecipeApp/DessertRecipeApp/ViewModels/RecipeDetail/RecipeDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeDetailViewModel.swift
+//  DessertRecipeApp
+//
+//  Created by Janine Chen on 10/26/23.
+//
+
+import Foundation
